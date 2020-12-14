c01a7f55d800ea66078c4727a36426d1c4a9df48
Lothar Haeger <lothar.haeger@is4it.de>
2020-12-14 10:00:41 +0100

    Change license to GPL-3

--- Launcher/AppDelegate.swift
+++ Launcher/AppDelegate.swift
@@ -1,10 +1,12 @@
 /*
  *
- *  Copyright (c) 2020 Stefan Post, Lothar Haeger
+ * AutoRaise
  *
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (c) 2020 Stefan Post, Lothar Haeger
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import CoreServices

be9e8053723b837819b92f0dc979116dfa3eb4bb
Stefan Post <s.post@thenewmotion.com>
2021-01-20 20:18:55 +0100

    Remove compilation step as the binary is already provided

--- create-app-bundle.sh
+++ create-app-bundle.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-g++ -O2 -Wall -fobjc-arc -o AutoRaise AutoRaise.mm -framework AppKit && \
 rm -rf AutoRaise.app &&
 mkdir -p AutoRaise.app/Contents/MacOS && \
 mkdir AutoRaise.app/Contents/Resources && \
 cp AutoRaise AutoRaise.app/Contents/MacOS && \
 cp Info.plist AutoRaise.app/Contents && \
 cp AutoRaise.icns AutoRaise.app/Contents/Resources && \
-chmod 700 AutoRaise.app
+chmod 700 AutoRaise.app && echo "Successfully created AutoRaise.app"
